52f40c903e3779a14c02026d951678a48b5525e7
andre28freitas-bit <andre28freitas@gmail.com>
2026-09-10 22:38:39 +0100

    Refresh PWA cache for cleaner progress deltas

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "traintrack-market-v14";
+const CACHE = "traintrack-market-v15";
 const APP_SHELL = ["./","./index.html","./styles.css","./extensions.css","./bootstrap.js","./config.js","./auth-redirect.js","./app.js","./extensions-core.js","./score-model.js","./extensions-calendar.js","./assessment-edit.js","./templates-progress-v2.js","./score-ui-patch.js","./summary-delta-ui.js","./calendar-nav-v2.js","./manifest.json","./icon.svg"];
 
 self.addEventListener("install", event => {
