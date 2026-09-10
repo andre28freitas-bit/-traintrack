4f1f0043c51b463c0be2c36a93d77b46d5862839
andre28freitas-bit <andre28freitas@gmail.com>
2026-09-10 17:06:59 +0100

    Refresh PWA cache for assessment editing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "traintrack-market-v8";
-const APP_SHELL = ["./","./index.html","./styles.css","./extensions.css","./bootstrap.js","./config.js","./auth-redirect.js","./app.js","./extensions-core.js","./extensions-calendar.js","./manifest.json","./icon.svg"];
+const CACHE = "traintrack-market-v9";
+const APP_SHELL = ["./","./index.html","./styles.css","./extensions.css","./bootstrap.js","./config.js","./auth-redirect.js","./app.js","./extensions-core.js","./extensions-calendar.js","./assessment-edit.js","./manifest.json","./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
